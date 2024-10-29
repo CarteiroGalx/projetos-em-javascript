1c9c12d7e2de5fcf846b7272202610e0633847a4
Eriver18 <almeidalopeserivelton@gmail.com>
2024-10-29 19:56:46 -0300

    FinalizAção da lógica da calculadora de IMC

--- 00-imc/imc.js
+++ 00-imc/imc.js
@@ -2,36 +2,44 @@ const calcular = document.getElementById('calcular');
 
 function imc () {
     const nome = document.getElementById('nome').value;
-    const altura = +document.getElementById('altura').value;
-    const peso = +document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
+
     if (nome !== '' && altura !== '' && peso !== '') {
-
-        const valorIMC = (peso / (altura * altura)).toFixed(1);
-
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
         let classificacao = '';
-
-        if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
-        }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
-        }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
-        }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau I.';
-        }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II';
-        }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
+        if(valorIMC < 18.5){
+            classificacao = "abaixo do peso."
+        }
+        else if(valorIMC >= 18.5 && valorIMC <= 24.9){
+            classificacao = "com peso normal."
+        }
+        else if(valorIMC >= 25 && valorIMC <= 29.9){
+            classificacao = "sobrepeso."
+        }
+        else if(valorIMC >= 30 && valorIMC <= 34.9){
+            classificacao = "com Obesidade Grau I"
+        }
+        else if(valorIMC >= 35 && valorIMC <= 39.9){
+            classificacao = "com Obesidade Grau II"
+        }
+        else if(valorIMC >= 40 && valorIMC <= 49.9){
+            classificacao = "com Obesidade Grau III"
+        }
+        else if(valorIMC >= 50 && valorIMC <= 59.9){
+            classificacao = "com Obesidade Grau IV"
+        }
+        else if(valorIMC > 59.9){
+            classificacao = "com Obesidade Grau V"
         }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-        
-    }else {
-        resultado.textContent = 'Preencha todos os campos!!!';
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
     }
-
+    else
+    {
+        resultado.textContent = "Preencha todos os campos!"
+    }
 }
-
 calcular.addEventListener('click', imc);
