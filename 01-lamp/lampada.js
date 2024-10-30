b39ab455d7a9007a3b635dadea680291684c8d66
Eriver18 <almeidalopeserivelton@gmail.com>
2024-10-29 23:24:07 -0300

    Retirando um dos botões e ainda mantendo funcionalidade de ligar e desligar

--- 01-lamp/lampada.js
+++ 01-lamp/lampada.js
@@ -1,33 +1,46 @@
-const turnOn = document.getElementById("turnOn");
-const turnOff = document.getElementById("turnOff");
+const button = document.getElementById("button");
 const lamp = document.getElementById("lamp");
 const text = document.getElementById("text");
 
 function isLampBroken(){
-    return lamp.src.indexOf('quebrada') > -1
+    return lamp.src.indexOf('quebrada') > -1;
 }
 
 function lampBroken(){
-    lamp.src = "./img/quebrada.jpg"
-    text.textContent = "Ai não! A lâmpada quebrou!"
+    lamp.src = "./img/quebrada.jpg";
+    text.textContent = "Ai não! A lâmpada quebrou!";
+    button.style.display = "none";
 }
 
 function lampOn(){
     if (!isLampBroken()){
-        lamp.src = "./img/ligada.jpg"
-        text.textContent = "A lâmpada está acesa!"
+        lamp.src = "./img/ligada.jpg";
+        text.textContent = "A lâmpada está acesa!";
+        button.textContent = "Desligar"
     }
 }
 
 function lampOff() {
     if (!isLampBroken()){
-        lamp.src = "./img/desligada.jpg"
-        text.textContent = "A lâmpada está apagada!"
+        lamp.src = "./img/desligada.jpg";
+        text.textContent = "A lâmpada está apagada!";
+        button.textContent = "Ligar"
     }
 }
 
-turnOn.addEventListener("click", lampOn);
-turnOff.addEventListener("click", lampOff);
+function lampAlternate(){
+    if(button.textContent == "Ligar"){
+        lampOn();
+        button.textContent = "Desligar";
+    }
+    else
+    {
+        lampOff();
+        button.textContent = "Ligar";
+    }
+}
+
+button.addEventListener("click", lampAlternate);
 lamp.addEventListener("mouseover", lampOn);
 lamp.addEventListener("mouseleave", lampOff);
 lamp.addEventListener("dblclick", lampBroken);
